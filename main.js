032bcd214b2bcf8bbda684eedbf71f29f6711820
fernando duenas <fernando39615@aim.com>
2020-10-13 12:55:19 -0500

    first and last

--- main.js
+++ main.js
@@ -2,8 +2,49 @@
  * YOUR CODE HERE *
  ******************/
 
+function onlyOdds(nums) {
+  const result = [];
 
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 1) {
+      result.push(nums[i]);
+    }
+  }
 
+  return result;
+}
+
+function onlyEvens(nums) {
+  const result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 !== 1) {
+      result.push(nums[i]);
+    }
+  }
+
+  return result;
+}
+
+function shortNamesOnly(namess) {
+  const result = [];
+  for (let i = 0; i < namess.length; i++) {
+    if (namess[i].length < 7) {
+      result.push(namess[i]);
+    }
+  }
+  return result;
+}
+
+function sNames(names) {
+  const result = [];
+  for (let i = 0; i < names.length; i++) {
+    if (names[i].charAt(0) == 'S') {
+      result.push(names[i]);
+    }
+  }
+  return result;
+}
 
 
 
